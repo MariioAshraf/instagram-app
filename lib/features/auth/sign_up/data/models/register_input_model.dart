740ec6d03c5430cd7537c0dbd24f03558d68321f
MarioAshraf <mariioashraf1@gmail.com>
2024-12-21 17:36:26 +0200

    - Created register input model - Created signup repo

---
+++ lib/features/auth/sign_up/data/models/register_input_model.dart
@@ -0,0 +1,7 @@
+class RegisterInputModel {
+  final String name;
+  final String password;
+  final String email;
+  RegisterInputModel(
+      {required this.name, required this.password, required this.email});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,17 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async{
   await dotenv.load();
+  await supabaseInitialization();
+  runApp(const MyApp());
+}
+
+Future<void> supabaseInitialization() async {
   String url = dotenv.env['PROJECT_URL'] ?? '';
   String anonKey = dotenv.env['API_KEY'] ?? '';
   await Supabase.initialize(
     url: url,
     anonKey: anonKey,
   );
-  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
