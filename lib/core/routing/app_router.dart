f6604550b8795b86d86a5fadce2c47c1bfecc606
MarioAshraf <mariioashraf1@gmail.com>
2024-12-23 17:56:19 +0200

    - Handled user Sign up functions - Added firebase auth and Firestore - Added sign up route in routes class - Used firebase istead of supabase in auth and store functionalities - Created saveUserData method in sign up use case - Handled UI for sign up succes, failure and loading - Handled sign up bloc listener - Added Controllers in sign up cubit - Created sign up method in cubit to perform with those controllers - hot fixes in sign up form

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_app/core/routing/routes.dart';
 
+import '../../features/auth/login/presentation/views/login_view.dart';
+import '../../features/auth/sign_up/presentation/views/sign_up_view.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
@@ -9,19 +11,20 @@ class AppRouter {
       //   return MaterialPageRoute(
       //     // builder: (_) => const OnBoardingView(),
       //   );
-      // case Routes.loginView: // here
-      //   return MaterialPageRoute(
-      //     // builder: (_) => const LoginView(),
-      //   );
+      case Routes.loginView: // here
+        return MaterialPageRoute(
+          builder: (_) => const LoginView(),
+        );
       // case Routes.homeView: // and here
       //   return MaterialPageRoute(
       //     // builder: (_) => const HomeView(),
       //   );
-      //   case Routes.signUpView: // and here
-      //   return MaterialPageRoute(
-      //     // builder: (_) => const SignUpView(),
-      //   );
-      default: return null;
+      case Routes.signUpView: // and here
+        return MaterialPageRoute(
+          builder: (_) => const SignUpView(),
+        );
+      default:
+        return null;
     }
   }
 }
