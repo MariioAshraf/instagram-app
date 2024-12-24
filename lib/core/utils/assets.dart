4a1361ff31afc623396419ec20f600a98d158206
MarioAshraf <mariioashraf1@gmail.com>
2024-12-25 00:58:41 +0200

    - Created home view app bar - Created create post container - create 2 containers for scaffold and create post LinearGradient color

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,4 @@
-class AssetsData{
+class AssetsData {
   static const homeTopBarLogo = 'assets/svgs/home_top_bar_image.svg';
+  static const homeTopBarShareIcon = 'assets/svgs/home_top_bar_share_icon.svg';
 }
