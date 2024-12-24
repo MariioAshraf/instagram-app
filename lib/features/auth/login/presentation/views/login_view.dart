d2831c373f973604e2dd32318c2f9d19604f4943
MarioAshraf <mariioashraf1@gmail.com>
2024-12-24 17:32:50 +0200

    - Created Login use case - Altered login repo functionalities to handle with firebase instead of supabase - Created getUserData method - Added login controllers in cubit - Handled login form with controllers - Edited DI cause of using use case

--- lib/features/auth/login/presentation/views/login_view.dart
+++ lib/features/auth/login/presentation/views/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:instagram_app/core/di/dependency_injection.dart';
+import 'package:instagram_app/features/auth/login/data/domain/user_cases/login_use_case.dart';
 import 'package:instagram_app/features/auth/login/presentation/views/widgets/login_view_body.dart';
 import '../../data/repos/login_repo_impl.dart';
 import '../manager/login_cubit.dart';
@@ -13,7 +14,7 @@ class LoginView extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: BlocProvider(
-          create: (context) => LoginCubit(getIt.get<LoginRepoImpl>()),
+          create: (context) => LoginCubit(getIt.get<LoginUseCase>()),
           child: const LoginViewBody(),
         ),
       ),
