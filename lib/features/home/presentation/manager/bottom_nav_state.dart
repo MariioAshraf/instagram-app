658e4d7d4e0ca4cf0a30a40dd74547427229ff66
MarioAshraf <mariioashraf1@gmail.com>
2024-12-26 19:13:55 +0200

    - Created bottom nav cubit - Added bottom nav bar in home - Added initial profile view - Added initial search view - Modified build user image

---
+++ lib/features/home/presentation/manager/bottom_nav_state.dart
@@ -0,0 +1,8 @@
+part of 'bottom_nav_cubit.dart';
+
+@immutable
+sealed class BottomNavState {}
+
+final class BottomNavInitial extends BottomNavState {}
+
+final class ChangeBottomNavState extends BottomNavState {}
---
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        backgroundColor: Colors.transparent,
+      body: Center(
+        child: Text('Profile'),
+      ),
+    );
+  }
+}
