4462e49676dafa5c80cdfc66e3411186b7b9d87e
MarioAshraf <mariioashraf1@gmail.com>
2025-02-18 05:40:05 +0200

    - Created story local data source - Created story remote data source - initial implementation  of get my stories method

---
+++ lib/features/story/data/data_sources/story_local_data_source.dart
@@ -0,0 +1,25 @@
+import 'package:hive/hive.dart';
+import 'package:instagram_app/constants.dart';
+
+import '../models/story_model.dart';
+
+abstract class StoryLocalDataSource {
+  List<StoryModel> getMyStories({required String userId});
+
+  List<StoryModel> getFriendsStories();
+}
+
+class StoryLocalDataSourceImpl implements StoryLocalDataSource {
+  @override
+  List<StoryModel> getFriendsStories() {
+    // TODO: implement getFriendsStories
+    throw UnimplementedError();
+  }
+
+  @override
+  List<StoryModel> getMyStories({required String userId}) {
+    var box = Hive.box<StoryModel>(kStories);
+    return box.values.where((story) => story.userId == userId).toList()
+      ..sort((a, b) => a.createdAt.compareTo(b.createdAt));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:instagram_app/features/profile/data/repos/profile_repo_impl.dart
 import 'package:instagram_app/features/profile/domain/use_cases/profile_use_case.dart';
 import 'package:instagram_app/features/story/data/repos/story_repo_impl.dart';
 import 'package:instagram_app/features/story/presentation/manager/story_cubit/story_cubit.dart';
+import 'constants.dart';
 import 'core/di/dependency_injection.dart';
 import 'core/utils/bloc_observer.dart';
 import 'core/routing/routes.dart';
@@ -27,6 +28,7 @@ void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(StoryModelAdapter());
   Hive.registerAdapter(MediaTypeAdapter());
+  await Hive.openBox<StoryModel>(kStories);
   setupServiceLocator();
   Bloc.observer = AppBlocObserver();
 
